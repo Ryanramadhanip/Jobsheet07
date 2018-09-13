bea622af403add0fc6e558c926f6334a56849337
Ryan Ramadhani <Ryan Ramadhani@DESKTOP-N31T9G9>
2018-09-13 12:48:18 +0700

    Initial Commit

--- src/Praktek/HitungFibonacci.java
+++ src/Praktek/HitungFibonacci.java
@@ -17,7 +17,7 @@ public class HitungFibonacci {
     {
         System.out.println("Identitas : " + identitas);;
         System.out.println("\nHitung Fibonacci");
-        System.out.println("l, 1, 2, 3, 5, 8, 13, 21, .,. dst.\n");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, .,. dst.\n");
     }
     
     public static void main(String[] args)
