0895171e6580e27b9f8cce2d645a63c10d20435b
Ryan Ramadhani <Ryan Ramadhani@DESKTOP-N31T9G9>
2018-09-14 21:46:57 +0700

    Tambah Input

--- src/Tugas/Tugas02.java
+++ src/Tugas/Tugas02.java
@@ -15,5 +15,12 @@ public class Tugas02 {
         String identitas = "Ryan Ramadhani Putra / X RPL 2 / 29";
         System.out.println("Identitas" + identitas);
         
+        Scanner baca = new Scanner(System.in);
+        int berat[]= {7, 5, 4, 3, 2};
+        int bayar[]= {10000, 7000, 5000, 3000, 2000};
+        int bayaran = 0;
+        
+        System.out.print("Jumlah berat maksimal : ");
+        int n = baca.nextInt();
     }
 }
