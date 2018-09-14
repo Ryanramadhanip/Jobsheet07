72748c04b9197e67f7d055630053f88a12e8cfc6
Ryan Ramadhani <Ryan Ramadhani@DESKTOP-N31T9G9>
2018-09-14 21:16:05 +0700

    Initial Commit

---
+++ src/Tugas/Tugas01.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas;
+import java.util.Scanner;
+/**
+ *
+ * @author Ryan Ramadhani
+ */
+public class Tugas01 {
+    public static void main(String[] args)
+    {
+        String identitas = "Ryan Ramadhani / X RPL 2 / 29";
+        System.out.println("Identitas = " + identitas);
+    }
+}
